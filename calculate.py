abf49c4545e445d94fff80c2c86fb0301896b0eb
Ankit952077 <116968419+Ankit952077@users.noreply.github.com>
2023-11-03 08:50:44 -0400

    calculate

---
+++ calculate.py
@@ -0,0 +1,13 @@
+a=int(input("enter num"))
+op=(input("enter  oprater + - * / "))
+b=int(input("enter nu2"))
+if(op=="+"):
+    print(a+b)
+elif(op=="-"):
+    print(a-b)
+elif(op=='*'):
+    print(a*b)
+elif(op=='/'):
+    print(a/b)
+else:
+    print("invalid")
